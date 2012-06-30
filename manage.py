49ed3fb438707de8bbc8546ca769622675821d2d
Ben Glynn <ben@potatolondon.com>
2012-06-30 17:30:19 +0100

    - It uses the PATH to find `dev_appserver` - It uses `dev_appserver` to modify the path

--- manage.py
+++ manage.py
@@ -1,34 +1,25 @@
 #!/usr/bin/env python
 
-import os, sys, subprocess
+import os, sys
 
-# Remind and django non-rel developers not to user `runserver`
+
+# User dev_appserver.py for now
 if 'runserver' in sys.argv:
     sys.stderr.write(
         "Error: please uses `dev_appserver.py` to run your development server\n")
     sys.exit(1)
 
-# Add appengine's paths (path list taken from dev_appserver.py)
-# todo, don't use `which` here, find a cross-platform way
-process = subprocess.Popen('readlink `which dev_appserver.py`', shell=True, stdout=subprocess.PIPE)
-path = os.path.abspath(os.path.dirname(os.path.realpath(process.stdout.read())))
 
-extra_paths = [
-  path,
-  # make sure django is first in case there are other django's on the system
-  os.path.join(path, 'lib', 'django_1_3'),
-  os.path.join(path, 'lib', 'antlr3'),
-  os.path.join(path, 'lib', 'fancy_urllib'),
-  os.path.join(path, 'lib', 'ipaddr'),
-  os.path.join(path, 'lib', 'protorpc'),
-  os.path.join(path, 'lib', 'webob'),
-  os.path.join(path, 'lib', 'webapp2'),
-  os.path.join(path, 'lib', 'yaml', 'lib'),
-  os.path.join(path, 'lib', 'simplejson'),
-  os.path.join(path, 'lib', 'google.appengine._internal.graphy'),
-]
-
-sys.path = extra_paths + sys.path
+# Set up the python path using dev_appserver
+for path in os.environ.get('PATH').split(os.pathsep):
+    if 'dev_appserver.py' in os.listdir(path):
+        sdk_path = os.path.dirname(
+            os.readlink(os.path.join(path, 'dev_appserver.py')))
+        sys.path.insert(0, sdk_path)
+        from dev_appserver import fix_sys_path
+        fix_sys_path()
+        # django 1.3 at top of path to obscure hobbled version
+        sys.path.insert(0, os.path.join(sdk_path, 'lib', 'django_1_3'))
 
 from django.core.management import execute_manager
 
