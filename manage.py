550ecd9e00e2355846f2abc1b083b6cc77f3913a
Ben Glynn <ben@potatolondon.com>
2012-07-03 14:07:26 +0100

    Abstracted path manipulations so that they can be reused. #24

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-import os, sys
+import sys
 sys.path.extend(['lib'])
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from pypath import pypath
+pypath()
 
 # Don't allow `runserver`
 if 'runserver' in sys.argv:
@@ -10,16 +12,6 @@ if 'runserver' in sys.argv:
         "Error: please uses `dev_appserver.py` to run your development server\n")
     sys.exit(1)
 
-# Set up the python path using dev_appserver
-for path in os.environ.get('PATH').split(os.pathsep):
-    if 'dev_appserver.py' in os.listdir(path):
-        sdk_path = os.path.dirname(
-            os.readlink(os.path.join(path, 'dev_appserver.py')))
-        sys.path.insert(0, sdk_path)
-        from dev_appserver import fix_sys_path
-        fix_sys_path()
-        # django 1.3 at top of path to obscure hobbled version
-        sys.path.insert(0, os.path.join(sdk_path, 'lib', 'django_1_3'))
 
 from django.core.management import execute_manager
 
