7301352ea457e5acc3dc3577701b9e9827cbb37b
Martin Allison <martin@potatolondon.com>
2012-11-09 14:41:24 +0000

    Change the hello message to something generic #16

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 import logging
+
 from django.views.generic import TemplateView
 from django.conf import settings
 
@@ -6,13 +7,9 @@ from django.conf import settings
 class HelloWorld(TemplateView):
     template_name = "hello-world.html"
     
-    def get(self, request, *args, **kwargs):
-        self.request.session['message'] = 'Sessions seem okay!'
-        return super(HelloWorld, self).get(request, *args, **kwargs)
-
     def get_context_data(self, **kwargs):
         context = super(HelloWorld, self).get_context_data(**kwargs)
-        context['message'] = self.request.session.get('message', None)
+        context['message'] = 'Hooray! Everything seems to work...'
         return context
 
 
