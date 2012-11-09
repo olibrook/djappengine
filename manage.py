6c35d1dad206dcff56c894c8a6812e64460d36fa
Martin Allison <martin@potatolondon.com>
2012-11-09 14:42:29 +0000

    Django 1.4 manage.py with new environ setup stuff #28

--- manage.py
+++ manage.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python
-
-from pypath import pypath
-pypath()
+import os
 import sys
 
-# Don't allow `runserver` just yet, we haven't wrapped it here
+from lib.environ import setup_environ
+setup_environ()
+
+# Don't allow `runserver` or `shell`
+
 if 'runserver' in sys.argv:
-    sys.stderr.write(
-        "Error: please uses `dev_appserver.py` to run your development server\n")
-    sys.exit(1)
+    # TODO: warn, but then run serve here as a subprocess?
+    print "You should serve your local instance with dev_appserver. See `serve.py`"
+    sys.exit(0)
 
 
-from django.core.management import execute_manager
+if 'shell' in sys.argv:
+    # TODO: warn, but then run `shell.py` as a subprocess?
+    print "See `shell.py` for a shell that works with the local data and sets up the right stubs"
+    sys.exit(0)
 
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    sys.stderr.write(
-        "Error: Can't find the file `settings.py` in the directory"
-        "containing %r. It appears you've customized things.\nYou'll have"
-        "to run django-admin.py, passing it your settings module.\n(If the"
-        "file `settings.py` does indeed exist, it's causing an ImportError"
-        "somehow.)\n" % __file__)
-    sys.exit(1)
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-if __name__ == "__main__": execute_manager(settings)
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
