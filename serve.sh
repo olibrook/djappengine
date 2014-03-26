5d8653894f68bab3f36f08aee31667e4e451a3f7
Martin Allison <mallison@google.com>
2014-03-26 12:09:21 +0000

    Update serve.sh to curry dev_appserver with some more default args. Create the tmp dir if it doesn't exist on first start up

--- serve.sh
+++ serve.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 
-dev_appserver.py . --log_level=debug --datastore_path=tmp/data
+HOST=0.0.0.0
+PORT=8080
+TMP_DIR=tmp
+
+if [ ! -d "$TMP_DIR" ] ; then
+    echo "Missing data directory: $TMP_DIR .. creating now";
+    mkdir $TMP_DIR
+fi
+
+if [ `command -v rlwrap` ]; then
+    CMD="rlwrap dev_appserver.py"
+else
+    CMD=dev_appserver.py
+fi
+
+$CMD app.yaml \
+    --log_level=info \
+    --skip_sdk_update_check \
+    --datastore_path=tmp/data \
+    --blobstore_path=tmp/blobstore \
+    --smtp_port=1025 \
+    --smtp_host=localhost \
+    --host=$HOST \
+    --port=$PORT \
+    --require_indexes=1 \
+    $@
