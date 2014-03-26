b89062519f78e4a201fd404deeabd0ce7ebf9e20
Martin Allison <mallison@google.com>
2014-03-26 12:08:15 +0000

    Remove session testing stuff from the test view

--- core/views.py
+++ core/views.py
@@ -1,13 +1,3 @@
 from django.views.generic import TemplateView
 
-
-class HelloWorld(TemplateView):
-    template_name = "hello-world.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(HelloWorld, self).get_context_data(**kwargs)
-        self.request.session['test'] = 'val'
-        context['session_test'] = self.request.session['test']
-        return context
-
-hello_world = HelloWorld.as_view()
+hello_world = TemplateView.as_view(template_name='hello-world.html')
