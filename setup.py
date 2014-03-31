46bcf65f59b40b8f08d4c51326f014be1728d3fa
Oliver Brook <olibrook@gmail.com>
2014-03-31 13:29:00 +0100

    Add setup.py, remove project template.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="djappengine",
+    version="0.1",
+    packages=find_packages(),
+    scripts=['memcache.py'],
+    description="",
+)
--- templates/project_name/appengine/fix_paths.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-sys.path[0:0] = [
-    os.path.join(os.path.dirname(__file__), 'src'),
-    os.path.join(os.path.dirname(__file__), 'lib'),
-]
