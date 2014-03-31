3d5f5918616f85d0b5466d9d01bb05f253284de9
Oliver Brook <olibrook@gmail.com>
2014-03-31 13:38:04 +0100

    Fix import

--- djappengine/sessions/urls.py
+++ djappengine/sessions/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
 
+import djappengine.sessions.views as views
+
 urlpatterns = patterns('',
-    url(r'^clean-up/$', 'views.session_clean_up', name='session-clean-up'),
+    url(r'^clean-up/$', views.session_clean_up, name='session-clean-up'),
 )
