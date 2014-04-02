177c4c7c7f9d316928a2cfa66355e6e73bfd7794
Oliver Brook <olibrook@gmail.com>
2014-04-02 13:58:08 +0100

    Added sandbox_shell script. Provides an interactive GAE environment.

---
+++ djappengine/tools/sandbox_shell.py
@@ -0,0 +1,66 @@
+import os
+import sys
+import code
+import contextlib
+
+
+import dev_appserver
+import wrapper_util
+
+
+# Initialize this script as though it were 'dev_appserver'. This needs to come before the
+# `dev_appserver` imports below.
+wrapper_util.reject_old_python_versions((2, 7))
+
+_DIR_PATH = wrapper_util.get_dir_path(dev_appserver.__file__, os.path.join('lib', 'ipaddr'))
+_PATHS = wrapper_util.Paths(_DIR_PATH)
+script_name = 'dev_appserver.py'
+sys.path = (_PATHS.script_paths(script_name) + _PATHS.scrub_path(script_name, sys.path))
+
+import google.appengine.tools.devappserver2.application_configuration as application_configuration
+import google.appengine.tools.devappserver2.devappserver2 as devappserver2
+import google.appengine.tools.devappserver2.python.sandbox as sandbox
+import google.appengine.tools.devappserver2.wsgi_request_info as wsgi_request_info
+
+
+@contextlib.contextmanager
+def appengine_environ():
+    """Set up and tear down an environment as though running an application through dev_appserver.
+
+    Usage example:
+
+    >>> import code
+    >>> with appengine_environ():
+    ...     code.interact()
+
+    """
+    options = devappserver2.PARSER.parse_args()
+    configuration = application_configuration.ApplicationConfiguration(options.config_paths)
+
+    storage_path = devappserver2._get_storage_path(options.storage_path, configuration.app_id)
+    dispatcher = None
+    request_data = wsgi_request_info.WSGIRequestInfo(dispatcher)
+
+    devappserver2._setup_environ(configuration.app_id)
+    apis = devappserver2.DevelopmentServer._create_api_server(request_data, storage_path, options, configuration)
+
+    # Enable App Engine libraries without enabling the full sandbox.
+    module = configuration.modules[0]
+    for l in sandbox._enable_libraries(module.normalized_libraries):
+        sys.path.insert(0, l)
+
+    apis.start()
+    try:
+        yield
+    finally:
+        apis.quit()
+
+
+def main():
+    """Start an interactive shell in an App Engine environment."""
+    with appengine_environ():
+        code.interact()
+
+
+if __name__ == '__main__':
+    main()
