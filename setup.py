2c4a8af234d319a52f21d7f32550f27a3253783f
Oliver Brook <olibrook@gmail.com>
2014-04-03 18:19:37 +0100

    Add entry points for get_refresh_token and sandbox_shell

--- setup.py
+++ setup.py
@@ -6,4 +6,13 @@ setup(
     packages=find_packages(),
     scripts=['memcache.py'],
     description="",
+    install_requires=[
+        'google-api-python-client>=1.2'
+    ],
+    entry_points={
+        'console_scripts': [
+            'get_refresh_token=djappengine.tools.get_refresh_token:main',
+            'sandbox_shell=djappengine.tools.sandbox_shell:main',
+        ],
+    }
 )
