63e3e469258baf21954bd7502b43821fa30b39bd
Oliver Brook <olibrook@gmail.com>
2014-04-04 11:16:58 +0100

    Updated documentation for get_refresh_token and sandbox_shell.

--- djappengine/tools/get_refresh_token.py
+++ djappengine/tools/get_refresh_token.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import argparse
 import sys
 
@@ -7,6 +8,51 @@ from oauth2client.file import Storage
 
 
 def main():
+    """Gets an oauth2 refresh token and saves the result to a file.
+
+    Many apps need to work with Google APIs in background tasks and use
+    `oauth2client.appengine.AppAssertionCredentials` to allow an app to own its
+    own documents, calendars, etc. This does not work on `dev_appserver`.
+
+    As a workaround during development, create a refresh token for the current
+    *developer* and let the app use this token instead. Each developer
+    should then set up his/her own resources on Google's APIs which the
+    development app can access without interfering with other developers.
+
+    This should be a one-time setup step for development and is independent
+    of any user-management/auth system in use.
+
+    Usage example with the Calendar API, after generating a token:
+
+        import httplib2
+
+        from google.appengine.api import memcache
+        from apiclient import discovery
+        from oauth2client.appengine import AppAssertionCredentials
+        from oauth2client import client
+
+        DEBUG = True
+        PATH_TO_CREDENTIALS = 'appengine/credentials.json'
+
+        def _get_credentials():
+            if DEBUG:
+                with open(PATH_TO_CREDENTIALS, 'r') as f:
+                    s = f.read()
+                return client.Credentials.new_from_json(s)
+            else:
+                return AppAssertionCredentials()
+
+        service = discovery.build(
+            serviceName='calendar',
+            version='v3',
+            developerKey=settings.GOOGLE_API_KEY,
+            http=_get_credentials().authorize(httplib2.Http(memcache))
+        )
+
+        calendar = service.calendars().get(calendarId='123').execute()
+
+
+    """
     argparser = argparse.ArgumentParser(
         description="Gets an oauth2 refresh token for a user and saves the result to a file.",
         parents=[tools.argparser],
--- djappengine/tools/sandbox_shell.py
+++ djappengine/tools/sandbox_shell.py
@@ -1,40 +1,56 @@
+#!/usr/bin/env python
 import os
 import sys
 import code
 import contextlib
-
+import argparse
 
 import dev_appserver
 import wrapper_util
 
 
-# Initialize this script as though it were 'dev_appserver'. This needs to come before the
-# `dev_appserver` imports below.
-wrapper_util.reject_old_python_versions((2, 7))
-
-_DIR_PATH = wrapper_util.get_dir_path(dev_appserver.__file__, os.path.join('lib', 'ipaddr'))
-_PATHS = wrapper_util.Paths(_DIR_PATH)
-script_name = 'dev_appserver.py'
-sys.path = (_PATHS.script_paths(script_name) + _PATHS.scrub_path(script_name, sys.path))
-
-import google.appengine.tools.devappserver2.application_configuration as application_configuration
-import google.appengine.tools.devappserver2.devappserver2 as devappserver2
-import google.appengine.tools.devappserver2.python.sandbox as sandbox
-import google.appengine.tools.devappserver2.wsgi_request_info as wsgi_request_info
+def main():
+    parser = argparse.ArgumentParser(
+        description="Start an interactive shell in an App Engine environment."
+    )
+    parser.add_argument('yaml_path')
+    args = parser.parse_args()
+    with appengine_environ(args.yaml_path):
+        code.interact()
 
 
 @contextlib.contextmanager
-def appengine_environ():
-    """Set up and tear down an environment as though running an application through dev_appserver.
+def appengine_environ(yaml_path):
+    """Set up and tear down an environment as though running an application
+    through `dev_appserver.py`.
 
-    Usage example:
+    Usage:
 
-    >>> import code
-    >>> with appengine_environ():
-    ...     code.interact()
+        with appengine_environ(yaml_path):
+            # Play with the datastore
+            pass
 
     """
-    options = devappserver2.PARSER.parse_args()
+
+    # Initialize as though we are running `dev_appserver.py`.
+
+    wrapper_util.reject_old_python_versions((2, 7))
+
+    _DIR_PATH = wrapper_util.get_dir_path(dev_appserver.__file__, os.path.join('lib', 'ipaddr'))
+    _PATHS = wrapper_util.Paths(_DIR_PATH)
+    script_name = 'dev_appserver.py'
+    sys.path = (_PATHS.script_paths(script_name) + _PATHS.scrub_path(script_name, sys.path))
+
+    # Initialize as though `dev_appserver.py` is about to run our app, using all the
+    # configuration provided in app.yaml.
+
+    import google.appengine.tools.devappserver2.application_configuration as application_configuration
+    import google.appengine.tools.devappserver2.devappserver2 as devappserver2
+    import google.appengine.tools.devappserver2.python.sandbox as sandbox
+    import google.appengine.tools.devappserver2.wsgi_request_info as wsgi_request_info
+
+    # The argparser is the easiest way to get the default options.
+    options = devappserver2.PARSER.parse_args([yaml_path])
     configuration = application_configuration.ApplicationConfiguration(options.config_paths)
 
     storage_path = devappserver2._get_storage_path(options.storage_path, configuration.app_id)
@@ -54,12 +70,6 @@ def appengine_environ():
         yield
     finally:
         apis.quit()
-
-
-def main():
-    """Start an interactive shell in an App Engine environment."""
-    with appengine_environ():
-        code.interact()
 
 
 if __name__ == '__main__':
